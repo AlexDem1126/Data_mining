7babf7379b12b08fbef204086c1cee828d9c0387
AlexDem1126 <alexdem1126@gmail.com>
2017-09-27 10:03:03 -0500

    main

---
+++ kmeans/myMain.java
@@ -0,0 +1,53 @@
+package kmeans;
+
+public class myMain {
+
+	public static void main(String[] args) {
+
+		if (args.length > 0 && args.length < 6) {															
+			try {
+				String F = args[0]; 					// F is a file name
+				int K = Integer.parseInt(args[1]); 		// K is a number of clusters
+				int I = Integer.parseInt(args[2]); 		// I is a max number of iterations in any run
+				double T = Double.parseDouble(args[3]); // T is a convergence threshold
+				int R = Integer.parseInt(args[4]); 		// R is a number of runs
+
+				System.out.println("F=" + F + " K=" + K + " I=" + I + " T=" + T + " R=" + R);	
+				
+				//get data from the file
+				manageFile_A objMF = new manageFile_A(F);			
+//				objMF.printFile(objMF.getDataset());
+				
+
+				
+				kmeans objKmeans = new kmeans(objMF.getDataset(), objMF.getNumOfPoints(), objMF.getNumOfDimension());
+				
+				
+				
+				//******************************************************************
+				/*********** Kmeans ***********/
+				//******************************************************************
+				int count = 0;
+				System.out.println("***** Kmeans *****");
+				while (R > 0) {
+					System.out.println("\nRun " + (count + 1) + "\n============================");
+					objKmeans.kMeanClustering(K, I, T, null); //cluster, iteration, threshold, centroid
+					R--;
+					count++;
+				}
+
+				
+				
+			} catch (NumberFormatException e) {
+				System.out.println(
+						"Input format should be: <file name.txt> or <file name.csv>");
+				System.exit(1);
+			}
+		} else {
+			System.out.println(
+					"Input format should be: <file name.txt> or <file name.csv>");
+		}	
+
+	}
+
+}
