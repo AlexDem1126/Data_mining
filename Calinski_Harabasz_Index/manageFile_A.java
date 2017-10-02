789eda41e394ca25b59c49ea6666e900cb04e056
AlexDem1126 <alexdem1126@gmail.com>
2017-10-02 08:43:12 -0500

    calinski_harabasz

--- Calinski_Harabasz_Index/manageFile_A.java
+++ Calinski_Harabasz_Index/manageFile_A.java
@@ -1,4 +1,4 @@
-package FP_AD_Fall2017;
+package calinski_harabasz;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -11,65 +11,59 @@ import java.util.regex.Pattern;
 public class manageFile_A {
 	public String fileName;
 	private int numOfPoints;
-	private int numOfDimension;	
-	private String[][] wineName;
+	private int numOfDimension;
+	
+	private int numOfTrueClusters;
+	private int numOfDimensionLable;
+	
+	private String[][] sequenceName;
 	private double[][] dataset;
-	private int[][] vintage;
-	private double[][] trueGrade;
+	private String[][] classDistribution;
 	
 	
-	
-			
-	public int getNumOfPoints() {
-		return numOfPoints;
-	}
-	public void setNumOfPoints(int numOfPoints) {
-		this.numOfPoints = numOfPoints;
-	}
-
-	
-	public int getNumOfDimension() {
-		return numOfDimension;
-	}
-	public void setNumOfDimension(int numOfDimension) {
-		this.numOfDimension = numOfDimension;
-	}
-
-	
-	public String[][] getWineName() {
-		return wineName;
-	}
-	public void setWineName(String[][] wineName) {
-		this.wineName = wineName;
-	}
-
-	
-	public int[][] getVintage() {
-		return vintage;
-	}
-	public void setVintage(int[][] vintage) {
-		this.vintage = vintage;
-	}
-
-	
-	public double[][] getTrueGrade() {
-		return trueGrade;
-	}
-	public void setTrueGrade(double[][] trueGrade) {
-		this.trueGrade = trueGrade;
-	}
-
 	
 	public double[][] getDataset() {
 		return dataset;
 	}
-	public void setDataset(double[][] dataset) {
-		this.dataset = dataset;
+
+	public void setDataset(double[][] dataset0) {
+		this.dataset = dataset0;
+	}	
+	
+	public int getNumOfTrueClusters() {
+		return numOfTrueClusters;
+	}
+
+	public void setNumOfTrueClusters(int numOfTrueClusters) {
+		this.numOfTrueClusters = numOfTrueClusters;
+	}
+
+	public int getNumOfDimensionLable() {
+		return numOfDimensionLable;
+	}
+
+	public void setNumOfDimensionLable(int numOfDimensionLable) {
+		this.numOfDimensionLable = numOfDimensionLable;
+	}
+
+	public int getNumOfPoints() {
+		return numOfPoints;
+	}
+
+	public void setNumOfPoints(int numOfPoints0) {
+		this.numOfPoints = numOfPoints0;
+	}
+
+	public int getNumOfDimension() {
+		return numOfDimension;
+	}
+
+	public void setNumOfDimension(int numOfDimension0) {
+		this.numOfDimension = numOfDimension0;
 	}
 
 	
 	
-
 public manageFile_A(String fileName2) {
 	fileName = fileName2;
 	BufferedReader br;
@@ -84,80 +78,70 @@ public manageFile_A(String fileName2) {
             int numOfColumns=0, numOfRows=1;
             
             while (br.readLine() != null) {
-            	String[] line_temp = line.split(",");            	
+            	String[] line_temp = line.split("  ");            	
             	numOfColumns = line_temp.length;				
 				numOfRows++;	
 			}
                         
             System.out.println("numOfRows: " + numOfRows);
-            System.out.println("numOfColumns: " + numOfColumns);            
-            br.close();            
+            System.out.println("numOfColumns: " + numOfColumns);
+            System.out.println(numOfRows + numOfColumns);
+            br.close();
             
             
-         // read whole file and store names in the wineName, data attributes in the dataset, years in vintage, labels in the trueGrade
+            
+         // read whole file and store names in the sequenceName, data attributes in the dataset, labels in the classDistribution
          			br = new BufferedReader(new FileReader(fileName));      
-         			fileData_L_temp =  new ArrayList<String>();         			
+         			fileData_L_temp =  new ArrayList<String>();
          			
-         			wineName = new String[numOfRows][];
+         			sequenceName = new String[numOfRows][];
          			for (int i = 0; i < numOfRows; i++) {
-//         				wineName[i] = new String[numOfColumns-307];	
-         				wineName[i] = new String[numOfColumns-7];	
+         				sequenceName[i] = new String[numOfColumns-8];				
         			}	
          			
          			dataset = new double[numOfRows][];
          			for (int i = 0; i < numOfRows; i++) {
-         				dataset[i] = new double[numOfColumns-3];				
+         				dataset[i] = new double[numOfColumns-2];				
         			}	
          			numOfPoints = numOfRows;
-         			numOfDimension = numOfColumns-3;
-
+         			numOfDimension = numOfColumns-2;
+	
          			
-         			vintage = new int[numOfRows][];
+         			classDistribution = new String[numOfRows][];
          			for (int i = 0; i < numOfRows; i++) {
-//         				vintage[i] = new int[numOfColumns-307];	
-         				vintage[i] = new int[numOfColumns-7];	
+         				classDistribution[i] = new String[numOfColumns-8];				
         			}	
-         			
-         			trueGrade = new double[numOfRows][];
-         			for (int i = 0; i < numOfRows; i++) {
-//         				trueGrade[i] = new double[numOfColumns-307];
-         				trueGrade[i] = new double[numOfColumns-7];
-        			}	       			
-
          			
          			int nrow = 0;
          			int ncol = 0;
          			
          			line = "";
          			double[] line2;
-//                    line = br.readLine();
                     while ((line = br.readLine()) != null) {
-                    	String[] line_temp2 = line.split(",");                    	
-                    	wineName[nrow][ncol] = line_temp2[0];
+                    	String[] line_temp2 = line.split("\\s+");
+                    	sequenceName[nrow][ncol] = line_temp2[0];
                     	
-                    	line2 = new double[line_temp2.length-3];
+                    	line2 = new double[line_temp2.length-2];
                     	
-                    	Pattern p = Pattern.compile("([0-1]{1,1})|([0-9]{4,4})|([0-9]{2,2})");
+                    	Pattern p = Pattern.compile("\\d\\.\\d+");
                     	Matcher m = p.matcher(line);
                     	int i2 = 0;
-                    	while(m.find() && (i2<=(numOfColumns-4))){ 
+                    	while(m.find()){
                     		line2[i2] = Double.parseDouble(m.group());   
                     		i2++;
                     	}
-                    	dataset[nrow] = line2;                     	
+                    	dataset[nrow] = line2;
 
-                    	vintage[nrow][ncol] = Integer.parseInt(line_temp2[6]);
-                    	trueGrade[nrow][ncol] = Double.parseDouble(line_temp2[7]); 
-//                    	vintage[nrow][ncol] = Integer.parseInt(line_temp2[306]);
-//                    	trueGrade[nrow][ncol] = Double.parseDouble(line_temp2[307]); 
-
-//                    	System.out.println(nrow+ "   "+wineName[nrow][ncol]+ "   "+vintage[nrow][ncol]+ "   "+trueGrade[nrow][ncol]);                    	
+                    	classDistribution[nrow][ncol] = line_temp2[8];
+//                    	System.out.println(sequenceName[nrow][ncol]+ "   "+classDistribution[nrow][ncol]);
+//                    	System.out.println(nrow);
                     nrow++;
                     }
         			//print data from file
 //        			printFile(dataset);
                     br.close();
-                    fileData_L_temp.clear();        
+                    fileData_L_temp.clear();
+           
 	
 		} catch (FileNotFoundException e) {
 			System.out.println("Can not find file " + fileName);
